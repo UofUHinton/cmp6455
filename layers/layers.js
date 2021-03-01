b0dc4d50e906323642c509162615727b264f2bc1
UofUHinton <78169414+UofUHinton@users.noreply.github.com>
2021-02-28 21:12:50 -0700

    Add files via upload

--- layers/layers.js
+++ layers/layers.js
@@ -1,21 +1,7 @@
 var wms_layers = [];
 
-var format_SLCo_County_Boundary_0 = new ol.format.GeoJSON();
-var features_SLCo_County_Boundary_0 = format_SLCo_County_Boundary_0.readFeatures(json_SLCo_County_Boundary_0, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_SLCo_County_Boundary_0 = new ol.source.Vector({
-    attributions: ' ',
-});
-jsonSource_SLCo_County_Boundary_0.addFeatures(features_SLCo_County_Boundary_0);
-var lyr_SLCo_County_Boundary_0 = new ol.layer.Vector({
-                declutter: true,
-                source:jsonSource_SLCo_County_Boundary_0, 
-                style: style_SLCo_County_Boundary_0,
-                interactive: true,
-                title: '<img src="styles/legend/SLCo_County_Boundary_0.png" /> SLCo_County_Boundary'
-            });
 
-        var lyr_OpenStreetMap_1 = new ol.layer.Tile({
+        var lyr_OpenStreetMap_0 = new ol.layer.Tile({
             'title': 'OpenStreetMap',
             'type': 'base',
             'opacity': 1.000000,
@@ -26,51 +12,111 @@ var lyr_SLCo_County_Boundary_0 = new ol.layer.Vector({
                 url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
             })
         });
-var format_LeftJoin_CountPerTract_2 = new ol.format.GeoJSON();
-var features_LeftJoin_CountPerTract_2 = format_LeftJoin_CountPerTract_2.readFeatures(json_LeftJoin_CountPerTract_2, 
+var format_SLCo_CensusTracts_1 = new ol.format.GeoJSON();
+var features_SLCo_CensusTracts_1 = format_SLCo_CensusTracts_1.readFeatures(json_SLCo_CensusTracts_1, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_LeftJoin_CountPerTract_2 = new ol.source.Vector({
+var jsonSource_SLCo_CensusTracts_1 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_LeftJoin_CountPerTract_2.addFeatures(features_LeftJoin_CountPerTract_2);
-var lyr_LeftJoin_CountPerTract_2 = new ol.layer.Vector({
+jsonSource_SLCo_CensusTracts_1.addFeatures(features_SLCo_CensusTracts_1);
+var lyr_SLCo_CensusTracts_1 = new ol.layer.Vector({
                 declutter: true,
-                source:jsonSource_LeftJoin_CountPerTract_2, 
-                style: style_LeftJoin_CountPerTract_2,
+                source:jsonSource_SLCo_CensusTracts_1, 
+                style: style_SLCo_CensusTracts_1,
                 interactive: true,
-    title: 'LeftJoin_CountPerTract<br />\
-    <img src="styles/legend/LeftJoin_CountPerTract_2_0.png" /> 0 - 0<br />\
-    <img src="styles/legend/LeftJoin_CountPerTract_2_1.png" /> 0 - 2<br />\
-    <img src="styles/legend/LeftJoin_CountPerTract_2_2.png" /> 2 - 5<br />\
-    <img src="styles/legend/LeftJoin_CountPerTract_2_3.png" /> 5 - 10<br />\
-    <img src="styles/legend/LeftJoin_CountPerTract_2_4.png" /> 10 - 17<br />'
-        });
-var format_SLCo_Healthcare_Facilities_3 = new ol.format.GeoJSON();
-var features_SLCo_Healthcare_Facilities_3 = format_SLCo_Healthcare_Facilities_3.readFeatures(json_SLCo_Healthcare_Facilities_3, 
+                title: '<img src="styles/legend/SLCo_CensusTracts_1.png" /> SLCo_CensusTracts'
+            });
+var format_Crashes_2015_2 = new ol.format.GeoJSON();
+var features_Crashes_2015_2 = format_Crashes_2015_2.readFeatures(json_Crashes_2015_2, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_SLCo_Healthcare_Facilities_3 = new ol.source.Vector({
+var jsonSource_Crashes_2015_2 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_SLCo_Healthcare_Facilities_3.addFeatures(features_SLCo_Healthcare_Facilities_3);
-var lyr_SLCo_Healthcare_Facilities_3 = new ol.layer.Vector({
+jsonSource_Crashes_2015_2.addFeatures(features_Crashes_2015_2);
+var lyr_Crashes_2015_2 = new ol.layer.Vector({
                 declutter: true,
-                source:jsonSource_SLCo_Healthcare_Facilities_3, 
-                style: style_SLCo_Healthcare_Facilities_3,
+                source:jsonSource_Crashes_2015_2, 
+                style: style_Crashes_2015_2,
                 interactive: true,
-                title: '<img src="styles/legend/SLCo_Healthcare_Facilities_3.png" /> SLCo_Healthcare_Facilities'
+                title: '<img src="styles/legend/Crashes_2015_2.png" /> Crashes_2015'
+            });
+var format_Crashes_2016_3 = new ol.format.GeoJSON();
+var features_Crashes_2016_3 = format_Crashes_2016_3.readFeatures(json_Crashes_2016_3, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_Crashes_2016_3 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_Crashes_2016_3.addFeatures(features_Crashes_2016_3);
+var lyr_Crashes_2016_3 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_Crashes_2016_3, 
+                style: style_Crashes_2016_3,
+                interactive: true,
+                title: '<img src="styles/legend/Crashes_2016_3.png" /> Crashes_2016'
+            });
+var format_Crashes_2017_4 = new ol.format.GeoJSON();
+var features_Crashes_2017_4 = format_Crashes_2017_4.readFeatures(json_Crashes_2017_4, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_Crashes_2017_4 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_Crashes_2017_4.addFeatures(features_Crashes_2017_4);
+var lyr_Crashes_2017_4 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_Crashes_2017_4, 
+                style: style_Crashes_2017_4,
+                interactive: true,
+                title: '<img src="styles/legend/Crashes_2017_4.png" /> Crashes_2017'
+            });
+var format_Crashes_2018_5 = new ol.format.GeoJSON();
+var features_Crashes_2018_5 = format_Crashes_2018_5.readFeatures(json_Crashes_2018_5, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_Crashes_2018_5 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_Crashes_2018_5.addFeatures(features_Crashes_2018_5);
+var lyr_Crashes_2018_5 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_Crashes_2018_5, 
+                style: style_Crashes_2018_5,
+                interactive: true,
+                title: '<img src="styles/legend/Crashes_2018_5.png" /> Crashes_2018'
+            });
+var format_Crashes_2019_6 = new ol.format.GeoJSON();
+var features_Crashes_2019_6 = format_Crashes_2019_6.readFeatures(json_Crashes_2019_6, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_Crashes_2019_6 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_Crashes_2019_6.addFeatures(features_Crashes_2019_6);
+var lyr_Crashes_2019_6 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_Crashes_2019_6, 
+                style: style_Crashes_2019_6,
+                interactive: true,
+                title: '<img src="styles/legend/Crashes_2019_6.png" /> Crashes_2019'
             });
 
-lyr_SLCo_County_Boundary_0.setVisible(true);lyr_OpenStreetMap_1.setVisible(true);lyr_LeftJoin_CountPerTract_2.setVisible(true);lyr_SLCo_Healthcare_Facilities_3.setVisible(true);
-var layersList = [lyr_SLCo_County_Boundary_0,lyr_OpenStreetMap_1,lyr_LeftJoin_CountPerTract_2,lyr_SLCo_Healthcare_Facilities_3];
-lyr_SLCo_County_Boundary_0.set('fieldAliases', {'pk': 'pk', 'objectid': 'objectid', 'countynbr': 'countynbr', 'entitynbr': 'entitynbr', 'entityyr': 'entityyr', 'name': 'name', 'fips': 'fips', 'stateplane': 'stateplane', 'pop_lastce': 'pop_lastce', 'pop_curres': 'pop_curres', 'globalid': 'globalid', 'fips_str': 'fips_str', 'color4': 'color4', 'shape_leng': 'shape_leng', 'shape_area': 'shape_area', });
-lyr_LeftJoin_CountPerTract_2.set('fieldAliases', {'objectid': 'objectid', 'geoid10': 'geoid10', 'total_count': 'total_count', 'Integer_Tot_Count': 'Integer_Tot_Count', });
-lyr_SLCo_Healthcare_Facilities_3.set('fieldAliases', {'pk': 'pk', 'objectid': 'objectid', 'factype': 'factype', 'facility_i': 'facility_i', 'facid': 'facid', 'name': 'name', 'address': 'address', 'city': 'city', 'zip': 'zip', 'telephone': 'telephone', 'type': 'type', 'county': 'county', 'telehealth': 'telehealth', 'functional': 'functional', });
-lyr_SLCo_County_Boundary_0.set('fieldImages', {'pk': 'TextEdit', 'objectid': 'TextEdit', 'countynbr': 'TextEdit', 'entitynbr': 'TextEdit', 'entityyr': 'TextEdit', 'name': 'TextEdit', 'fips': 'TextEdit', 'stateplane': 'TextEdit', 'pop_lastce': 'TextEdit', 'pop_curres': 'TextEdit', 'globalid': 'TextEdit', 'fips_str': 'TextEdit', 'color4': 'TextEdit', 'shape_leng': 'TextEdit', 'shape_area': 'TextEdit', });
-lyr_LeftJoin_CountPerTract_2.set('fieldImages', {'objectid': 'TextEdit', 'geoid10': 'TextEdit', 'total_count': 'TextEdit', 'Integer_Tot_Count': 'Range', });
-lyr_SLCo_Healthcare_Facilities_3.set('fieldImages', {'pk': '', 'objectid': '', 'factype': '', 'facility_i': '', 'facid': '', 'name': '', 'address': '', 'city': '', 'zip': '', 'telephone': '', 'type': '', 'county': '', 'telehealth': '', 'functional': '', });
-lyr_SLCo_County_Boundary_0.set('fieldLabels', {'pk': 'no label', 'objectid': 'no label', 'countynbr': 'no label', 'entitynbr': 'no label', 'entityyr': 'no label', 'name': 'no label', 'fips': 'no label', 'stateplane': 'no label', 'pop_lastce': 'no label', 'pop_curres': 'no label', 'globalid': 'no label', 'fips_str': 'no label', 'color4': 'no label', 'shape_leng': 'no label', 'shape_area': 'no label', });
-lyr_LeftJoin_CountPerTract_2.set('fieldLabels', {'objectid': 'no label', 'geoid10': 'no label', 'total_count': 'no label', 'Integer_Tot_Count': 'no label', });
-lyr_SLCo_Healthcare_Facilities_3.set('fieldLabels', {'pk': 'no label', 'objectid': 'no label', 'factype': 'header label', 'facility_i': 'no label', 'facid': 'no label', 'name': 'header label', 'address': 'no label', 'city': 'header label', 'zip': 'no label', 'telephone': 'no label', 'type': 'no label', 'county': 'no label', 'telehealth': 'no label', 'functional': 'no label', });
-lyr_SLCo_Healthcare_Facilities_3.on('precompose', function(evt) {
+lyr_OpenStreetMap_0.setVisible(true);lyr_SLCo_CensusTracts_1.setVisible(true);lyr_Crashes_2015_2.setVisible(true);lyr_Crashes_2016_3.setVisible(true);lyr_Crashes_2017_4.setVisible(true);lyr_Crashes_2018_5.setVisible(true);lyr_Crashes_2019_6.setVisible(true);
+var layersList = [lyr_OpenStreetMap_0,lyr_SLCo_CensusTracts_1,lyr_Crashes_2015_2,lyr_Crashes_2016_3,lyr_Crashes_2017_4,lyr_Crashes_2018_5,lyr_Crashes_2019_6];
+lyr_SLCo_CensusTracts_1.set('fieldAliases', {'pk': 'pk', 'objectid': 'objectid', 'statefp10': 'statefp10', 'countyfp10': 'countyfp10', 'tractce10': 'tractce10', 'geoid10': 'geoid10', 'name10': 'name10', 'funcstat10': 'funcstat10', 'intptlat10': 'intptlat10', 'intptlon10': 'intptlon10', 'logrecno': 'logrecno', 'arealand': 'arealand', 'areawatr': 'areawatr', 'pop100': 'pop100', 'hu100': 'hu100', 'p0020001': 'p0020001', 'p0020002': 'p0020002', 'p0020003': 'p0020003', 'p0020004': 'p0020004', 'p0020005': 'p0020005', 'p0020006': 'p0020006', 'p0020007': 'p0020007', 'p0020008': 'p0020008', 'p0020009': 'p0020009', 'p0020010': 'p0020010', 'mtfcc': 'mtfcc', 'p0010011': 'p0010011', 'p0010012': 'p0010012', 'p0010013': 'p0010013', 'p0010014': 'p0010014', 'sqmiles': 'sqmiles', 'shape_leng': 'shape_leng', 'shape_area': 'shape_area', });
+lyr_Crashes_2015_2.set('fieldAliases', {'pk': 'pk', 'objectid': 'objectid', 'County_nam': 'County_nam', 'pedestrian': 'pedestrian', 'GCS_long || gcs_lat': 'GCS_long || gcs_lat', 'year': 'year', 'X': 'X', 'Y': 'Y', });
+lyr_Crashes_2016_3.set('fieldAliases', {'pk': 'pk', 'objectid': 'objectid', 'County_nam': 'County_nam', 'pedestrian': 'pedestrian', 'GCS_long || gcs_lat': 'GCS_long || gcs_lat', 'year': 'year', 'X': 'X', 'Y': 'Y', });
+lyr_Crashes_2017_4.set('fieldAliases', {'pk': 'pk', 'objectid': 'objectid', 'County_nam': 'County_nam', 'pedestrian': 'pedestrian', 'GCS_long || gcs_lat': 'GCS_long || gcs_lat', 'year': 'year', 'X': 'X', 'Y': 'Y', });
+lyr_Crashes_2018_5.set('fieldAliases', {'pk': 'pk', 'objectid': 'objectid', 'County_nam': 'County_nam', 'pedestrian': 'pedestrian', 'GCS_long || gcs_lat': 'GCS_long || gcs_lat', 'year': 'year', 'X': 'X', 'Y': 'Y', });
+lyr_Crashes_2019_6.set('fieldAliases', {'pk': 'pk', 'objectid': 'objectid', 'County_nam': 'County_nam', 'pedestrian': 'pedestrian', 'GCS_long || gcs_lat': 'GCS_long || gcs_lat', 'year': 'year', 'X': 'X', 'Y': 'Y', });
+lyr_SLCo_CensusTracts_1.set('fieldImages', {'pk': 'TextEdit', 'objectid': 'TextEdit', 'statefp10': 'TextEdit', 'countyfp10': 'TextEdit', 'tractce10': 'TextEdit', 'geoid10': 'TextEdit', 'name10': 'TextEdit', 'funcstat10': 'TextEdit', 'intptlat10': 'TextEdit', 'intptlon10': 'TextEdit', 'logrecno': 'TextEdit', 'arealand': 'TextEdit', 'areawatr': 'TextEdit', 'pop100': 'TextEdit', 'hu100': 'TextEdit', 'p0020001': 'TextEdit', 'p0020002': 'TextEdit', 'p0020003': 'TextEdit', 'p0020004': 'TextEdit', 'p0020005': 'TextEdit', 'p0020006': 'TextEdit', 'p0020007': 'TextEdit', 'p0020008': 'TextEdit', 'p0020009': 'TextEdit', 'p0020010': 'TextEdit', 'mtfcc': 'TextEdit', 'p0010011': 'TextEdit', 'p0010012': 'TextEdit', 'p0010013': 'TextEdit', 'p0010014': 'TextEdit', 'sqmiles': 'TextEdit', 'shape_leng': 'TextEdit', 'shape_area': 'TextEdit', });
+lyr_Crashes_2015_2.set('fieldImages', {'pk': '', 'objectid': '', 'County_nam': '', 'pedestrian': '', 'GCS_long || gcs_lat': '', 'year': '', 'X': '', 'Y': '', });
+lyr_Crashes_2016_3.set('fieldImages', {'pk': '', 'objectid': '', 'County_nam': '', 'pedestrian': '', 'GCS_long || gcs_lat': '', 'year': '', 'X': '', 'Y': '', });
+lyr_Crashes_2017_4.set('fieldImages', {'pk': '', 'objectid': '', 'County_nam': '', 'pedestrian': '', 'GCS_long || gcs_lat': '', 'year': '', 'X': '', 'Y': '', });
+lyr_Crashes_2018_5.set('fieldImages', {'pk': '', 'objectid': '', 'County_nam': '', 'pedestrian': '', 'GCS_long || gcs_lat': '', 'year': '', 'X': '', 'Y': '', });
+lyr_Crashes_2019_6.set('fieldImages', {'pk': '', 'objectid': '', 'County_nam': '', 'pedestrian': '', 'GCS_long || gcs_lat': '', 'year': '', 'X': '', 'Y': '', });
+lyr_SLCo_CensusTracts_1.set('fieldLabels', {'pk': 'no label', 'objectid': 'no label', 'statefp10': 'no label', 'countyfp10': 'no label', 'tractce10': 'no label', 'geoid10': 'no label', 'name10': 'no label', 'funcstat10': 'no label', 'intptlat10': 'no label', 'intptlon10': 'no label', 'logrecno': 'no label', 'arealand': 'no label', 'areawatr': 'no label', 'pop100': 'no label', 'hu100': 'no label', 'p0020001': 'no label', 'p0020002': 'no label', 'p0020003': 'no label', 'p0020004': 'no label', 'p0020005': 'no label', 'p0020006': 'no label', 'p0020007': 'no label', 'p0020008': 'no label', 'p0020009': 'no label', 'p0020010': 'no label', 'mtfcc': 'no label', 'p0010011': 'no label', 'p0010012': 'header label', 'p0010013': 'no label', 'p0010014': 'no label', 'sqmiles': 'no label', 'shape_leng': 'no label', 'shape_area': 'no label', });
+lyr_Crashes_2015_2.set('fieldLabels', {'pk': 'no label', 'objectid': 'no label', 'County_nam': 'no label', 'pedestrian': 'no label', 'GCS_long || gcs_lat': 'no label', 'year': 'no label', 'X': 'no label', 'Y': 'no label', });
+lyr_Crashes_2016_3.set('fieldLabels', {'pk': 'no label', 'objectid': 'no label', 'County_nam': 'no label', 'pedestrian': 'no label', 'GCS_long || gcs_lat': 'no label', 'year': 'no label', 'X': 'no label', 'Y': 'no label', });
+lyr_Crashes_2017_4.set('fieldLabels', {'pk': 'no label', 'objectid': 'no label', 'County_nam': 'no label', 'pedestrian': 'no label', 'GCS_long || gcs_lat': 'no label', 'year': 'no label', 'X': 'no label', 'Y': 'no label', });
+lyr_Crashes_2018_5.set('fieldLabels', {'pk': 'no label', 'objectid': 'no label', 'County_nam': 'no label', 'pedestrian': 'no label', 'GCS_long || gcs_lat': 'no label', 'year': 'no label', 'X': 'no label', 'Y': 'no label', });
+lyr_Crashes_2019_6.set('fieldLabels', {'pk': 'no label', 'objectid': 'no label', 'County_nam': 'no label', 'pedestrian': 'no label', 'GCS_long || gcs_lat': 'no label', 'year': 'no label', 'X': 'no label', 'Y': 'no label', });
+lyr_Crashes_2019_6.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
--- resources/qgis2web.js
+++ resources/qgis2web.js
@@ -1,5 +1,47 @@
 
 
+var measuring = false;
+var measureControl = (function (Control) {
+    measureControl = function(opt_options) {
+
+      var options = opt_options || {};
+
+      var button = document.createElement('button');
+      button.className += ' fas fa-ruler ';
+
+      var this_ = this;
+      var handleMeasure = function(e) {
+        if (!measuring) {
+            this_.getMap().addInteraction(draw);
+            createHelpTooltip();
+            createMeasureTooltip();
+            measuring = true;
+        } else {
+            this_.getMap().removeInteraction(draw);
+            measuring = false;
+            this_.getMap().removeOverlay(helpTooltip);
+            this_.getMap().removeOverlay(measureTooltip);
+        }
+      };
+
+      button.addEventListener('click', handleMeasure, false);
+      button.addEventListener('touchstart', handleMeasure, false);
+
+      var element = document.createElement('div');
+      element.className = 'measure-control ol-unselectable ol-control';
+      element.appendChild(button);
+
+      ol.control.Control.call(this, {
+        element: element,
+        target: options.target
+      });
+
+    };
+    if (Control) measureControl.__proto__ = Control;
+    measureControl.prototype = Object.create(Control && Control.prototype);
+    measureControl.prototype.constructor = measureControl;
+    return measureControl;
+}(ol.control.Control));
 var container = document.getElementById('popup');
 var content = document.getElementById('popup-content');
 var closer = document.getElementById('popup-closer');
@@ -20,7 +62,7 @@ var expandedAttribution = new ol.control.Attribution({
 
 var map = new ol.Map({
     controls: ol.control.defaults({attribution:false}).extend([
-        expandedAttribution
+        expandedAttribution,new measureControl()
     ]),
     target: document.getElementById('map'),
     renderer: 'canvas',
@@ -34,7 +76,7 @@ var map = new ol.Map({
 var layerSwitcher = new ol.control.LayerSwitcher({tipLabel: "Layers"});
 map.addControl(layerSwitcher);
 
-map.getView().fit([-12479910.267159, 4958537.899606, -12436238.218574, 4983687.544854], map.getSize());
+map.getView().fit([-12540521.506343, 4914799.138971, -12368974.563464, 5012685.443761], map.getSize());
 
 var NO_POPUP = 0
 var ALL_FIELDS = 1
@@ -75,7 +117,7 @@ var featureOverlay = new ol.layer.Vector({
 });
 
 var doHighlight = false;
-var doHover = true;
+var doHover = false;
 
 var highlight;
 var autolinker = new Autolinker({truncate: {length: 30, location: 'smart'}});
@@ -339,6 +381,26 @@ var onSingleClick = function(evt) {
 };
 
 
+    map.on('pointermove', function(evt) {
+        if (evt.dragging) {
+            return;
+        }
+        if (measuring) {
+            /** @type {string} */
+            var helpMsg = 'Click to start drawing';
+            if (sketch) {
+                var geom = (sketch.getGeometry());
+                if (geom instanceof ol.geom.Polygon) {
+                    helpMsg = continuePolygonMsg;
+                } else if (geom instanceof ol.geom.LineString) {
+                    helpMsg = continueLineMsg;
+                }
+            }
+            helpTooltipElement.innerHTML = helpMsg;
+            helpTooltip.setPosition(evt.coordinate);
+        }
+    });
+    
 
 map.on('pointermove', function(evt) {
     onPointerMove(evt);
@@ -347,6 +409,201 @@ map.on('singleclick', function(evt) {
     onSingleClick(evt);
 });
 
+/**
+ * Currently drawn feature.
+ * @type {ol.Feature}
+ */
+
+/**
+ * The help tooltip element.
+ * @type {Element}
+ */
+var helpTooltipElement;
+
+
+/**
+ * Overlay to show the help messages.
+ * @type {ol.Overlay}
+ */
+var helpTooltip;
+
+
+/**
+ * The measure tooltip element.
+ * @type {Element}
+ */
+var measureTooltipElement;
+
+
+/**
+ * Overlay to show the measurement.
+ * @type {ol.Overlay}
+ */
+var measureTooltip;
+
+
+/**
+ * Message to show when the user is drawing a line.
+ * @type {string}
+ */
+var continueLineMsg = 'Click to continue drawing the line';
+
+
+
+
+
+
+var source = new ol.source.Vector();
+
+var measureLayer = new ol.layer.Vector({
+    source: source,
+    style: new ol.style.Style({
+        fill: new ol.style.Fill({
+            color: 'rgba(255, 255, 255, 0.2)'
+        }),
+        stroke: new ol.style.Stroke({
+            color: '#ffcc33',
+            width: 3
+        }),
+        image: new ol.style.Circle({
+            radius: 7,
+            fill: new ol.style.Fill({
+                color: '#ffcc33'
+            })
+        })
+    })
+});
+
+map.addLayer(measureLayer);
+
+var draw; // global so we can remove it later
+function addInteraction() {
+  var type = 'LineString';
+  draw = new ol.interaction.Draw({
+    source: source,
+    type: /** @type {ol.geom.GeometryType} */ (type),
+    style: new ol.style.Style({
+      fill: new ol.style.Fill({
+        color: 'rgba(255, 255, 255, 0.2)'
+      }),
+      stroke: new ol.style.Stroke({
+        color: 'rgba(0, 0, 0, 0.5)',
+        lineDash: [10, 10],
+        width: 2
+      }),
+      image: new ol.style.Circle({
+        radius: 5,
+        stroke: new ol.style.Stroke({
+          color: 'rgba(0, 0, 0, 0.7)'
+        }),
+        fill: new ol.style.Fill({
+          color: 'rgba(255, 255, 255, 0.2)'
+        })
+      })
+    })
+  });
+
+  var listener;
+  draw.on('drawstart',
+      function(evt) {
+        // set sketch
+        sketch = evt.feature;
+
+        /** @type {ol.Coordinate|undefined} */
+        var tooltipCoord = evt.coordinate;
+
+        listener = sketch.getGeometry().on('change', function(evt) {
+          var geom = evt.target;
+          var output;
+            output = formatLength( /** @type {ol.geom.LineString} */ (geom));
+            tooltipCoord = geom.getLastCoordinate();
+          measureTooltipElement.innerHTML = output;
+          measureTooltip.setPosition(tooltipCoord);
+        });
+      }, this);
+
+  draw.on('drawend',
+      function(evt) {
+        measureTooltipElement.className = 'tooltip tooltip-static';
+        measureTooltip.setOffset([0, -7]);
+        // unset sketch
+        sketch = null;
+        // unset tooltip so that a new one can be created
+        measureTooltipElement = null;
+        createMeasureTooltip();
+        ol.Observable.unByKey(listener);
+      }, this);
+}
+
+
+/**
+ * Creates a new help tooltip
+ */
+function createHelpTooltip() {
+  if (helpTooltipElement) {
+    helpTooltipElement.parentNode.removeChild(helpTooltipElement);
+  }
+  helpTooltipElement = document.createElement('div');
+  helpTooltipElement.className = 'tooltip hidden';
+  helpTooltip = new ol.Overlay({
+    element: helpTooltipElement,
+    offset: [15, 0],
+    positioning: 'center-left'
+  });
+  map.addOverlay(helpTooltip);
+}
+
+
+/**
+ * Creates a new measure tooltip
+ */
+function createMeasureTooltip() {
+  if (measureTooltipElement) {
+    measureTooltipElement.parentNode.removeChild(measureTooltipElement);
+  }
+  measureTooltipElement = document.createElement('div');
+  measureTooltipElement.className = 'tooltip tooltip-measure';
+  measureTooltip = new ol.Overlay({
+    element: measureTooltipElement,
+    offset: [0, -15],
+    positioning: 'bottom-center'
+  });
+  map.addOverlay(measureTooltip);
+}
+
+
+function convertToFeet(length) {
+    feet_length = length * 3.2808;
+    return feet_length
+}
+
+/**
+ * format length output
+ * @param {ol.geom.LineString} line
+ * @return {string}
+ */
+var formatLength = function(line) {
+  var length;
+  var coordinates = line.getCoordinates();
+  length = 0;
+  var sourceProj = map.getView().getProjection();
+  for (var i = 0, ii = coordinates.length - 1; i < ii; ++i) {
+      var c1 = ol.proj.transform(coordinates[i], sourceProj, 'EPSG:4326');
+      var c2 = ol.proj.transform(coordinates[i + 1], sourceProj, 'EPSG:4326');
+      length += ol.sphere.getDistance(c1, c2);
+    }
+    feet_length = convertToFeet(length)
+
+    var output;
+    if (feet_length > 5280) {
+        output = (Math.round(feet_length / 5280 * 100) / 100) + ' miles';
+    } else {
+        output = (Math.round(feet_length * 100) / 100) + ' ft';
+    }
+    return output;
+};
+
+addInteraction();
 
 
 
